c5855779810b2ac1a085469723b181b25090d074
nester77 <alex.nester2477@gmail.com>
2022-12-24 20:56:23 +0300

    LMS-3: Add User object

---
+++ src/main/java/academy/belhard/lms/model/User.java
@@ -0,0 +1,34 @@
+package academy.belhard.lms.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(schema = "lms", name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "role")
+    private Role role;
+}
