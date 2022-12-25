4bc45bab2286738b233d5026d2c84b370f24cd6b
UladzislauM <marshal25@yandex.by>
2022-12-25 20:25:26 +0300

    LMS-2: Add User to UserDto and back

--- src/main/java/academy/belhard/lms/service/impl/ObjectMapperR.java
+++ src/main/java/academy/belhard/lms/service/impl/ObjectMapperR.java
@@ -1,7 +1,9 @@
 package academy.belhard.lms.service.impl;
 
 import academy.belhard.lms.data.entity.Request;
+import academy.belhard.lms.data.entity.User;
 import academy.belhard.lms.service.dto.RequestDto;
+import academy.belhard.lms.service.dto.UserDto;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -24,5 +26,33 @@ public class ObjectMapperR {
         requestDto.setDeleted(request.isDeleted());
         requestDto.setStatusReq(request.getStatusReq());
         return requestDto;
+    }
+
+    public User toUser(UserDto userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setName(userDTO.getName());
+        user.setLast_name(userDTO.getLast_name());
+        user.setRole(userDTO.getRole());
+        user.setIs_active(userDTO.getIs_active());
+        user.setRequest(userDTO.getRequest());
+        user.setCourse_list(userDTO.getCourse_list());
+        return user;
+    }
+
+    public UserDto toUserDTO(User user) {
+        UserDto userDTO = new UserDto();
+        userDTO.setId(user.getId());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setName(user.getName());
+        userDTO.setLast_name(user.getLast_name());
+        userDTO.setRole(user.getRole());
+        userDTO.setIs_active(user.getIs_active());
+        userDTO.setRequest(user.getRequest());
+        userDTO.setCourse_list(user.getCourse_list());
+        return userDTO;
     }
 }
