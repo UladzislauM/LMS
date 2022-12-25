0a3847248d6c5cb410735b5058dae6c14eb7cee8
UladzislauM <marshal25@yandex.by>
2022-12-25 22:47:14 +0300

    LMS-2: Add SQL scheme & data

--- SQL/Schema.sql
+++ SQL/Schema.sql
@@ -1,4 +1,7 @@
 /*
+DROP TABLE IF EXISTS lessons;
+DROP TABLE IF EXISTS requests;
+DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS users;
 */
 
@@ -11,3 +14,26 @@ CREATE TABLE IF NOT EXISTS users(
  role	            CHARACTER VARYING (60),
  is_active			BOOLEAN NOT NULL DEFAULT TRUE
 );
+
+CREATE TABLE IF NOT EXISTS courses(
+ id                 BIGSERIAL PRIMARY KEY NOT NULL,
+ title				CHARACTER VARYING (80) NOT NULL,
+ description		CHARACTER VARYING (200),
+ price				DECIMAL(10,2),
+ start_date			DATE NOT null,
+ trainer			CHARACTER VARYING (60) NOT NULL
+ );
+
+CREATE TABLE IF NOT EXISTS requests(
+ id                 BIGSERIAL PRIMARY KEY NOT NULL,
+ courses_id			BIGINT REFERENCES courses(id),
+ users_id			BIGINT REFERENCES users(id),
+ status				CHARACTER VARYING (60),
+ is_deleted			BOOLEAN NOT NULL DEFAULT FALSE
+ );
+ 
+ CREATE TABLE IF NOT EXISTS lessons(
+ id                 BIGSERIAL PRIMARY KEY NOT NULL,
+ title				CHARACTER VARYING (80) NOT NULL,
+ courses_id			BIGINT REFERENCES courses(id)
+ );
--- SQL/data.sql
+++ SQL/data.sql
@@ -1,4 +1,39 @@
+<<<<<<< HEAD
 INSERT INTO users(email, password, first_name, last_name, patronymic_name, contact_preferences, social_media)
 VALUES ('ivan99@gmail.com', 'jfhd86', 'Ivan', 'Ivanov', 'Ivanovich', 'TELEGRAM', '@Ivan'),
        ('petr48@gmail.com', 'fdfbbfd67sdb', 'Petr', 'Petrov', 'Petrovich', 'CELLPHONE', '+375291234567'),
        ('sidor@gmail.com', 'klkjjk', 'Sidor', 'Sidorov', 'Sidorovich', 'INSTAGRAM', '#Sidor');
+=======
+INSERT INTO users(name, last_name, email, password, role) 
+VALUES ('Yauheni', 'Hlaholeu', 'jek94@gmail.com', '12qwaszx', 'ADMIN'),
+    ('Uladzislau', 'Solovev', 'sol44@yandex.by', 'qazxsw21', 'USER'),
+    ('Haliana', 'Sidoric', 'galina_sid@gmail.com', 'sid93LL', 'USER'),
+    ('Lana', 'Dimidova', 'dlana@mail.ru', 'vfAz1234', 'MANAGER'),
+    ('Andrey', 'Aksenov', 'AKsin@Gmail.com','12345678OOp', 'USER'),
+    ('Nazar', 'Vahtongov', 'vagan@mail.ru', '333eeeddfd', 'USER'),
+    ('Tatyana', 'Minikova', 'tMin@tut.by', 'trewrg', 'USER');
+    
+INSERT INTO courses (title, description, price, start_date, trainer) 
+VALUES ('course_test_1', 'description_test_1', 111, '2000-01-01', 'test_name_1'),
+	('course_test_2', 'description_test_2', 121, '2000-01-02', 'test_name_2'),
+	('course_test_3', 'description_test_3', 112, '2000-01-03', 'test_name_3'),
+	('course_test_4', 'description_test_4', 211, '2000-01-04', 'test_name_4'),
+	('course_test_5', 'description_test_5', 131, '2000-01-05', 'test_name_5'),
+	('course_test_6', 'description_test_6', 113, '2000-01-06', 'test_name_6');
+
+INSERT INTO requests (courses_id, users_id, status) 
+VALUES ((SELECT id FROM courses WHERE title = 'course_test_1'), (SELECT id FROM users WHERE name = 'Yauheni' AND last_name ='Hlaholeu'), 'STUDENT'),
+((SELECT id FROM courses WHERE title = 'course_test_2'), (SELECT id FROM users WHERE name = 'Uladzislau' AND last_name ='Solovev'), 'TRAINER'),
+((SELECT id FROM courses WHERE title = 'course_test_3'), (SELECT id FROM users WHERE name = 'Lana' AND last_name ='Dimidova'), 'STUDENT'),
+((SELECT id FROM courses WHERE title = 'course_test_4'), (SELECT id FROM users WHERE name = 'Andrey' AND last_name ='Aksenov'), 'STUDENT'),
+((SELECT id FROM courses WHERE title = 'course_test_5'), (SELECT id FROM users WHERE name = 'Haliana' AND last_name ='Sidoric'), 'STUDENT'),
+((SELECT id FROM courses WHERE title = 'course_test_6'), (SELECT id FROM users WHERE name = 'Nazar' AND last_name ='Vahtongov'), 'STUDENT'),
+((SELECT id FROM courses WHERE title = 'course_test_6'), (SELECT id FROM users WHERE name = 'Tatyana' AND last_name ='Minikova'), 'MANAGER');
+
+INSERT INTO lessons (courses_id, title) 
+VALUES ((SELECT id FROM courses WHERE title = 'course_test_1'), 'test_lesson_title_1'),
+((SELECT id FROM courses WHERE title = 'course_test_1'), 'test_lesson_title_2'),
+((SELECT id FROM courses WHERE title = 'course_test_2'), 'test_lesson_title_3'),
+((SELECT id FROM courses WHERE title = 'course_test_3'), 'test_lesson_title_4'),
+((SELECT id FROM courses WHERE title = 'course_test_4'), 'test_lesson_title_5');
+>>>>>>> LMS-2: Add SQL scheme & data
