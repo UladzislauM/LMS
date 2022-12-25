a8bef1aa45721249af5f60363139f8ae9cf1cd79
UladzislauM <marshal25@yandex.by>
2022-12-25 08:40:15 +0300

    LMS-2: Add emun StatusReq

---
+++ src/main/java/academy/belhard/lms/data/entity/StatusReq.java
@@ -0,0 +1,14 @@
+package academy.belhard.lms.data.entity;
+
+public enum StatusReq {
+    IN_PROCESSING,
+    ASSEMBLED,
+    AWAITING_PAYMENT,
+    READY_TO_START,
+    PAID_FOR,
+    COMPLETELY_CHANGED,
+    FAILED,
+    REFUND,
+    THE_TRANSACTION_IS_COMPLETED,
+    CANCELLED
+}
