9c580c10ec5d5907d1db380bf92ed03334f5a184
UladzislauM <marshal25@yandex.by>
2022-12-25 17:58:05 +0300

    LMS-2: Add RequestService

---
+++ src/main/java/academy/belhard/lms/service/RequestService.java
@@ -0,0 +1,9 @@
+package academy.belhard.lms.service;
+
+import academy.belhard.lms.data.entity.Request;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface RequestService extends AbstractService<Request> {
+
+}
