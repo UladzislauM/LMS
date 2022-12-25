bc60efcf98420f693e691a42c05595bb3f810b2c
UladzislauM <marshal25@yandex.by>
2022-12-25 08:29:46 +0300

    LMS-2: Add emun RoleUser

---
+++ src/main/java/academy/belhard/lms/data/entity/RoleUser.java
@@ -0,0 +1,12 @@
+package academy.belhard.lms.data.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RoleUser {
+    STUDENT,
+    MANAGER,
+    TRAINER
+}
