ad247f78076984eb273929018c5df9f70d2af5fc
UladzislauM <marshal25@yandex.by>
2022-12-25 17:56:19 +0300

    LMS-2: Add AbstractService

---
+++ src/main/java/academy/belhard/lms/service/AbstractService.java
@@ -0,0 +1,18 @@
+package academy.belhard.lms.service;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface AbstractService<D> {
+    List<D> findAll();
+
+    D findById(Long id);
+
+    void delete(Long id);
+
+    D create(D d);
+
+    D update(D d);
+}
