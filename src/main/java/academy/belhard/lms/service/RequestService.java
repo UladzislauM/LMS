06a8d0a34564d7af9951b49224cbde87fe9e7202
UladzislauM <marshal25@yandex.by>
2022-12-25 18:16:40 +0300

    LMS-2: Changed Request to RequestDto

--- src/main/java/academy/belhard/lms/service/RequestService.java
+++ src/main/java/academy/belhard/lms/service/RequestService.java
@@ -1,9 +1,9 @@
 package academy.belhard.lms.service;
 
-import academy.belhard.lms.data.entity.Request;
+import academy.belhard.lms.service.dto.RequestDto;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface RequestService extends AbstractService<Request> {
+public interface RequestService extends AbstractService<RequestDto> {
 
 }
