3a53489df3cf126a0058bdf7e09fc9dd8e32e402
UladzislauM <marshal25@yandex.by>
2022-12-25 17:25:54 +0300

    LMS-2: Add implementation Request

--- src/main/java/academy/belhard/lms/data/repository/RequestRepImpl.java
+++ src/main/java/academy/belhard/lms/data/repository/RequestRepImpl.java
@@ -1,2 +1,42 @@
-package academy.belhard.lms.data.repository;public class RequestRepImpl {
+package academy.belhard.lms.data.repository;
+
+import academy.belhard.lms.data.repository.impl.RequestRep;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("request_rep")
+@Transactional
+public class RequestRepImpl implements RequestRep {
+
+    private static final String GET_ALL = """
+            FROM Request
+            """;
+
+    @Override
+    public Optional findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public Object create(Object entity) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object entity) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
 }
