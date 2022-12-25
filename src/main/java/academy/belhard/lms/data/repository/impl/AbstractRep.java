24ac3f39e47670f0df624030850a6558a30b18a5
UladzislauM <marshal25@yandex.by>
2022-12-25 17:23:55 +0300

    LMS-2: Add interfaces Repository

--- src/main/java/academy/belhard/lms/data/repository/impl/AbstractRep.java
+++ src/main/java/academy/belhard/lms/data/repository/impl/AbstractRep.java
@@ -1,2 +1,19 @@
-package academy.belhard.lms.data.repository.impl;public interface AbstractRep {
+package academy.belhard.lms.data.repository.impl;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public interface AbstractRep<T> {
+    Optional<T> findById(Long id);
+
+    List<T> findAll();
+
+    T create(T entity);
+
+    T update(T entity);
+
+    boolean delete(Long id);
 }
--- src/main/java/academy/belhard/lms/data/repository/impl/RequestRep.java
+++ src/main/java/academy/belhard/lms/data/repository/impl/RequestRep.java
@@ -1,2 +1,8 @@
-package academy.belhard.lms.data.repository.impl;public interface RequestRep {
+package academy.belhard.lms.data.repository.impl;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface RequestRep extends AbstractRep{
+
 }
