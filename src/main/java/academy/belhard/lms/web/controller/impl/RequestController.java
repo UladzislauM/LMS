876918e733c26c47614a3fa483150ee156b509a7
UladzislauM <marshal25@yandex.by>
2022-12-26 15:56:55 +0300

    LMS-2: Add users to model for tests

--- src/main/java/academy/belhard/lms/web/controller/impl/RequestController.java
+++ src/main/java/academy/belhard/lms/web/controller/impl/RequestController.java
@@ -1,6 +1,7 @@
 package academy.belhard.lms.web.controller.impl;
 
 import academy.belhard.lms.service.RequestService;
+import academy.belhard.lms.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class RequestController {
     private final RequestService requestService;
+    private final UserService userService;
 
     @GetMapping("/request_find")
     public String findRequests(Model model) {
+        model.addAttribute("users", userService.findAll());
         model.addAttribute("requests", requestService.findAll());
         return "request";
     }
+
+
 }
