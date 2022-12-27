fb1d2c6666f6cee65168efee4c40858c42b941ac
UladzislauM <marshal25@yandex.by>
2022-12-27 20:02:06 +0300

    LMS-2: Add request find by id

--- src/main/java/academy/belhard/lms/web/controller/impl/RequestController.java
+++ src/main/java/academy/belhard/lms/web/controller/impl/RequestController.java
@@ -1,12 +1,16 @@
 package academy.belhard.lms.web.controller.impl;
 
+import academy.belhard.lms.data.entity.Course;
+import academy.belhard.lms.data.entity.Lesson;
 import academy.belhard.lms.service.RequestService;
 import academy.belhard.lms.service.UserService;
+import academy.belhard.lms.service.dto.CourseDto;
+import academy.belhard.lms.service.dto.LessonDto;
+import academy.belhard.lms.service.dto.RequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/request")
@@ -22,5 +26,10 @@ public class RequestController {
         return "request";
     }
 
+    @GetMapping("/request_find_by_id/{id}")
+    public String findRequestById(@PathVariable Long id, Model model){
+        model.addAttribute("request", requestService.findById(id));
+        return "request_by_id";
+    }
 
 }
