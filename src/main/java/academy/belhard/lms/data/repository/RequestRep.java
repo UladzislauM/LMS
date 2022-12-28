a01409d53d55651096f0a250788896da21e01950
UladzislauM <marshal25@yandex.by>
2022-12-28 20:46:01 +0300

    LMS-2: delete dependency

--- src/main/java/academy/belhard/lms/data/repository/RequestRep.java
+++ src/main/java/academy/belhard/lms/data/repository/RequestRep.java
@@ -4,8 +4,6 @@ import academy.belhard.lms.data.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public interface RequestRep extends JpaRepository<Request, Long> {
 
