beb4277db01aec32ee69d2d1d9a5d0a9d862fae4
UladzislauM <marshal25@yandex.by>
2022-12-28 16:05:23 +0300

    LMS-2: Add REST controller

---
+++ src/main/java/academy/belhard/lms/controller/rest/RequestControllerREST.java
@@ -0,0 +1,52 @@
+package academy.belhard.lms.controller.rest;
+
+import academy.belhard.lms.data.entity.Request;
+import academy.belhard.lms.service.CourseService;
+import academy.belhard.lms.service.RequestService;
+import academy.belhard.lms.service.dto.RequestDto;
+import academy.belhard.lms.service.impl.ObjectMapperR;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/request")
+@RequiredArgsConstructor
+public class RequestControllerREST {
+    private final RequestService requestService;
+    private final CourseService courseService;
+    private final ObjectMapperR mapper;
+
+    @PostMapping("/create")
+    public Request create(@RequestBody RequestDto requestDto) {
+        return mapper.toRequest(requestService.create(requestDto));
+    }
+
+    @GetMapping("/find_all")
+    public List<Request> findRequests() {
+        List<RequestDto> requestsDto = requestService.findAll();
+        return requestsDto.stream()
+                .map(mapper::toRequest)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/find_by_id/{id}")
+    public Request findRequestById(@PathVariable Long id) {
+        return mapper.toRequest(requestService.findById(id));
+    }
+
+
+    @PutMapping("/update/{id}")
+    public Request updateRequest(@PathVariable Long id, @RequestBody RequestDto requestDto) {
+        return mapper.toRequest(requestService.update(requestDto));
+    }
+
+    @GetMapping("/delete/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        requestService.delete(id);
+    }
+}
+
