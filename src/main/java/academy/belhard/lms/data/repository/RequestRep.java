938d5b079e905019042b5d73251825996cef8b90
UladzislauM <marshal25@yandex.by>
2022-12-28 20:13:55 +0300

    LMS-2: Rebase on JpaRepository & Delete classes

--- src/main/java/academy/belhard/lms/data/repository/RequestRep.java
+++ src/main/java/academy/belhard/lms/data/repository/RequestRep.java
@@ -1,11 +1,12 @@
 package academy.belhard.lms.data.repository;
 
 import academy.belhard.lms.data.entity.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
-public interface RequestRep extends AbstractRep<Request> {
-    Optional<Request> findByCourseId (Long id);
+public interface RequestRep extends JpaRepository<Request, Long> {
+
 }
