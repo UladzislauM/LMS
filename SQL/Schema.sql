6c871229fd50dd552d39ab0686c5800a20486e89
UladzislauM <marshal25@yandex.by>
2022-12-29 14:58:02 +0300

    LMS-2: Change SQL for 3 Entity

--- SQL/Schema.sql
+++ SQL/Schema.sql
@@ -1,9 +1,23 @@
 /*
 DROP TABLE IF EXISTS requests;
+DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS users;
 */
+
+CREATE TABLE IF NOT EXISTS users(
+ id                 BIGSERIAL PRIMARY KEY NOT NULL,
+ email              CHARACTER VARYING (100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS courses(
+ id                 BIGSERIAL PRIMARY KEY NOT NULL,
+ title				CHARACTER VARYING (80) NOT NULL
+ );
 
 CREATE TABLE IF NOT EXISTS requests(
  id                 BIGSERIAL PRIMARY KEY NOT NULL,
+ courses_id			BIGINT REFERENCES courses(id),
+ users_id			BIGINT REFERENCES users(id),
  status				CHARACTER VARYING (60),
  is_deleted			BOOLEAN NOT NULL DEFAULT FALSE
  );
