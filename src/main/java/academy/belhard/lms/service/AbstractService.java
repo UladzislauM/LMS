066104394193c7414c149781611d97e50def6c32
UladzislauM <marshal25@yandex.by>
2022-12-31 12:50:34 +0300

    LMS-2: Change return type ro Page<>

--- src/main/java/academy/belhard/lms/service/AbstractService.java
+++ src/main/java/academy/belhard/lms/service/AbstractService.java
@@ -1,12 +1,14 @@
 package academy.belhard.lms.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public interface AbstractService<D> {
-    List<D> getAll();
+    Page<D> getAll(Pageable pageable);
 
     D getById(Long id);
 
