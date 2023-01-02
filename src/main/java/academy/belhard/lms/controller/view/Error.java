5b4788f1bb56b25c071c1ae7ab44a75ae399136c
UladzislauM <marshal25@yandex.by>
2023-01-02 12:58:07 +0300

    LMS-6: Create error controller

---
+++ src/main/java/academy/belhard/lms/controller/view/Error.java
@@ -0,0 +1,40 @@
+package academy.belhard.lms.controller.view;
+
+import academy.belhard.lms.service.exception.LmsException;
+import academy.belhard.lms.service.exception.NotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ControllerAdvice
+@RequestMapping("/error")
+public class Error {
+    @GetMapping
+    public String error(){
+        return "error";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String internalServerError(LmsException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String conflictError(LmsException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFoundError(NotFoundException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+}
