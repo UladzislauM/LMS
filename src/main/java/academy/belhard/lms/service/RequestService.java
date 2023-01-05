731f95e26586fede17ba6d0077a3915eb47e43f1
UladzislauM <marshal25@yandex.by>
2023-01-05 17:25:16 +0300

    LMS-6: Delete annotation @Component

--- src/main/java/academy/belhard/lms/service/RequestService.java
+++ src/main/java/academy/belhard/lms/service/RequestService.java
@@ -3,12 +3,10 @@ package academy.belhard.lms.service;
 import academy.belhard.lms.service.dto.request.RequestDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
 import java.util.List;
 
-@Component
 public interface RequestService {
     Page<RequestDto> getAll(Pageable pageable);
 
