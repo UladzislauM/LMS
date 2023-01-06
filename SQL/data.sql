9ba36dad5f6b481cfa29cb86ce5084d839a80078
Herman Rabinkin <herman2lv@gmail.com>
2023-01-06 10:58:40 +0300

    LMS-11: Update sql scripts

--- SQL/data.sql
+++ SQL/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO users(email, password, first_name, last_name, patronymic_name, contact_preferences, social_media)
 VALUES ('ivan99@gmail.com', 'jfhd86', 'Ivan', 'Ivanov', 'Ivanovich', 'TELEGRAM', '@Ivan'),
-('petr48@gmail.com', 'fdfbbfd67sdb', 'Petr', 'Petrov', 'Petrovich', 'CELLPHONE', '+375291234567'),
-('sidor@gmail.com', 'klkjjk', 'Sidor', 'Sidorov', 'Sidorovich', 'INSTAGRAM', '#Sidor');
+       ('petr48@gmail.com', 'fdfbbfd67sdb', 'Petr', 'Petrov', 'Petrovich', 'CELLPHONE', '+375291234567'),
+       ('sidor@gmail.com', 'klkjjk', 'Sidor', 'Sidorov', 'Sidorovich', 'INSTAGRAM', '#Sidor');
--- SQL/scheme.sql
+++ SQL/scheme.sql
@@ -1,12 +1,14 @@
-CREATE TABLE users (
-   id BIGSERIAL PRIMARY KEY NOT NULL,
-   email CHARACTER VARYING (100) NOT NULL,
-   password CHARACTER VARYING (50) NOT NULL,
-   first_name CHARACTER VARYING (100) NOT NULL,
-   last_name CHARACTER VARYING (100) NOT NULL,
-   patronymic_name CHARACTER VARYING (100),
-   contact_preferences CHARACTER VARYING (50) NOT NULL,
-   social_media CHARACTER VARYING (50) NOT NULL,
-   role CHARACTER VARYING (50) NOT NULL,
-   is_active BOOLEAN NOT NULL
-  );
+-- DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users
+(
+    id                  BIGSERIAL PRIMARY KEY  NOT NULL,
+    email               CHARACTER VARYING(100) NOT NULL,
+    password            CHARACTER VARYING(50)  NOT NULL,
+    first_name          CHARACTER VARYING(100) NOT NULL,
+    last_name           CHARACTER VARYING(100) NOT NULL,
+    patronymic_name     CHARACTER VARYING(100),
+    contact_preferences CHARACTER VARYING(50)  NOT NULL,
+    social_media        CHARACTER VARYING(50),
+    role                CHARACTER VARYING(50)  NOT NULL,
+    is_active           BOOLEAN                NOT NULL DEFAULT TRUE
+);
