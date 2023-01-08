ad6fcb3166d4545457dcbd98d00802ae6c3928a1
UladzislauM <marshal25@yandex.by>
2023-01-08 11:30:49 +0300

    LMS-6: Delete fields - deleted, status, id

--- src/main/java/academy/belhard/lms/service/dto/request/RequestDtoForSave.java
+++ src/main/java/academy/belhard/lms/service/dto/request/RequestDtoForSave.java
@@ -4,11 +4,7 @@ import academy.belhard.lms.service.dto.user.UserDtoForSaving;
 import lombok.Data;
 
 @Data
-public class RequestDtoForSaving {
-    private Long id;
+public class RequestDtoForSave {
     private CourseDto course;
     private UserDtoForSaving user;
-    private StatusReqDto statusReq;
-    private boolean deleted;
 }
-
