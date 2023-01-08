2e108e9fef8fe2429f2afade763b79a2af454eaa
UladzislauM <marshal25@yandex.by>
2023-01-08 11:27:44 +0300

    LMS-6: Change parameters for object Pageable

--- src/main/java/academy/belhard/lms/controller/view/RequestControllerWeb.java
+++ src/main/java/academy/belhard/lms/controller/view/RequestControllerWeb.java
@@ -2,69 +2,50 @@ package academy.belhard.lms.controller.view;
 
 import academy.belhard.lms.service.RequestService;
 import academy.belhard.lms.service.dto.request.RequestDto;
-import academy.belhard.lms.service.dto.request.RequestDtoForSaving;
+import academy.belhard.lms.service.dto.request.RequestDtoForSave;
+import academy.belhard.lms.service.dto.request.RequestDtoForUpdate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @Controller
-@RequestMapping("/request")
+@RequestMapping("/requests")
 @RequiredArgsConstructor
 public class RequestControllerWeb {
     private final RequestService requestService;
 
-    @GetMapping("/")
-    public String getRequests(Model model, @RequestParam(required = false) Optional<Integer> page,
-                              @RequestParam(required = false) Optional<Integer> size) {
-        int currentPage = page.orElse(1);
-        int pageSize = size.orElse(5);
-        Page<RequestDto> requestDtoPage = requestService.getAll(
-                PageRequest.of(currentPage - 1, pageSize));
-        model.addAttribute("requests", requestDtoPage);
-        int totalPages = requestDtoPage.getTotalPages();
-        model.addAttribute("pageNumbers", requestService.getPageNumbers(model, totalPages));
+    @PostMapping
+    public String create(@ModelAttribute RequestDtoForSave request, Pageable pageable) {
+        requestService.create(request);
+        return "redirect:/requests?size=" + pageable.getPageSize() + "&page=" + pageable.getPageNumber();
+    }
+
+    @GetMapping
+    public String getAll(Model model, Pageable pageable) {
+        Page<RequestDto> requests = requestService.getAll(pageable);
+        model.addAttribute("requests", requests);
         return "request";
     }
 
     @GetMapping("/{id}")
-    public String getRequestById(@PathVariable Long id, Model model,
-                                 @RequestParam(value = "size", required = false, defaultValue = "5") String size,
-                                 @RequestParam(value = "page", required = false, defaultValue = "1") String page) {
+    public String getById(@PathVariable Long id, Model model) {
         model.addAttribute("request", requestService.getById(id));
-        model.addAttribute("size", size);
-        model.addAttribute("page", page);
         return "request_by_id";
     }
 
-
-    @PostMapping("/")
-    public String createRequest(@ModelAttribute RequestDtoForSaving request,
-                                @RequestParam(value = "size", required = false, defaultValue = "5") String size,
-                                @RequestParam(value = "page", required = false, defaultValue = "1") String page) {
-        requestService.create(request);
-        return "redirect:/request/?size=" + size + "&page=" + page;
-    }
-
     @PostMapping("/{id}")
-    public String editRequest(@ModelAttribute RequestDto request,
-                              @RequestParam(value = "size", required = false, defaultValue = "5") String size,
-                              @RequestParam(value = "page", required = false, defaultValue = "1") String page) {
+    public String update(@ModelAttribute RequestDtoForUpdate request) {
         requestService.update(request);
-        return "redirect:/request/?size=" + size + "&page=" + page;
+        return "redirect:/requests?sort=id,asc";
     }
 
     @GetMapping("/update_form/{id}")
-    public String toUpdateForm(@PathVariable Long id, Model model,
-                               @RequestParam(value = "size", required = false, defaultValue = "5") String size,
-                               @RequestParam(value = "page", required = false, defaultValue = "1") String page) {
+    public String toUpdateForm(@PathVariable Long id, Model model) {
         model.addAttribute("request", requestService.getById(id));
-        model.addAttribute("size", size);
-        model.addAttribute("page", page);
         return "update_request";
     }
 
