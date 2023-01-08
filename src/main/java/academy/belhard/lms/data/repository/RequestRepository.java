50a2eb4386b4e67876ed57581e04df10ba38b17e
UladzislauM <marshal25@yandex.by>
2023-01-08 11:32:03 +0300

    LMS-6: Delete annotation & rename to RequestRepository

--- src/main/java/academy/belhard/lms/data/repository/RequestRepository.java
+++ src/main/java/academy/belhard/lms/data/repository/RequestRepository.java
@@ -2,9 +2,7 @@ package academy.belhard.lms.data.repository;
 
 import academy.belhard.lms.data.entity.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-@Component
-public interface RequestRep extends JpaRepository<Request, Long> {
+public interface RequestRepository extends JpaRepository<Request, Long> {
 
 }
