e1627ee38e1a1808e338c02ec5a7be3a709dc4a9
Aminaev Roman <romanzovut@gmail.com>
2023-01-10 10:39:35 +0300

    LMS-17 Test for UserService.getAllUsers.

--- src/test/java/academy/belhard/lms/service/impl/RequestServiceImplTest.java
+++ src/test/java/academy/belhard/lms/service/impl/RequestServiceImplTest.java
@@ -2,6 +2,7 @@ package academy.belhard.lms.service.impl;
 
 import academy.belhard.lms.service.UserService;
 import academy.belhard.lms.service.dto.user.UserDto;
+import academy.belhard.lms.service.dto.user.UserDtoForSaving;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -15,25 +16,33 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
 class RequestServiceImplTest {
 
-    @MockBean
-    private UserService userService;
+    UserService userServiceMock = Mockito.mock(UserService.class);
+
+    UserDto userDtoMock = Mockito.mock(UserDto.class);
 
     @MockBean
     private UserDto userDto;
 
+
+//    @MockBean
+//    private UserService userService;
+
+//    @MockBean
+//    private UserDto userDto;
+//  UserDto createUser(UserDtoForSaving userDtoForSaving);
     @Test
     void create() {
     }
 
     @Test
-    void getAll() {
+    void getAllUsers() {
         List<UserDto> list = new ArrayList<>();
-        list.add(userDto);
-        Mockito.when(userService.getAllUsers()).thenReturn(list);
-        assertEquals(list, userService.getAllUsers());
+        list.add(userDtoMock);
+        Mockito.when(userServiceMock.getAllUsers()).thenReturn(list);
+        assertEquals(list, userServiceMock.getAllUsers());
     }
 
     @Test
