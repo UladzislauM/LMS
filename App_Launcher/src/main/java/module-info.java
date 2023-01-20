1ce9fc568135ce1c301d0d85354f3d7cdaebfb7d
Herman Rabinkin <herman2lv@gmail.com>
2023-01-20 13:11:21 +0300

    LMS-15: remove module-info.java files

--- App_Launcher/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module lms.launcher {
-    requires spring.boot;
-    requires spring.boot.autoconfigure;
-    requires spring.core;
-    requires spring.beans;
-    requires spring.context;
-    requires java.sql;
-}
