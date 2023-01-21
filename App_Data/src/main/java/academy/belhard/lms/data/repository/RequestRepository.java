d74ba12cfdfe703c9eb90a686f3f2c627bfbdce1
UladzislauM <marshal25@yandex.by>
2023-01-21 08:51:55 +0300

    LMS-36 Refactor Create, Update methods

--- App_Data/src/main/java/academy/belhard/lms/data/repository/RequestRepository.java
+++ App_Data/src/main/java/academy/belhard/lms/data/repository/RequestRepository.java
@@ -4,6 +4,8 @@ import academy.belhard.lms.data.entity.Request;
 import academy.belhard.lms.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RequestRepository extends JpaRepository<Request, Long> {
-    Request findByUser(User user);
+    Optional<Request> findByUser(User user);
 }
