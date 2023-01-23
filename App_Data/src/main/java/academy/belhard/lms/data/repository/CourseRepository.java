97777c143edf88b826640794c525735c5e225113
Pavel_Vysotski <Pavel_Vysotski@epam.com>
2023-01-23 12:06:24 +0300

    add templates

--- App_Data/src/main/java/academy/belhard/lms/data/repository/CourseRepository.java
+++ App_Data/src/main/java/academy/belhard/lms/data/repository/CourseRepository.java
@@ -5,14 +5,14 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
     @Modifying
     @Query("update Course c set c.deleted = true where c.id = :id")
-    void deleteCourseById(Long id);
+    void deleteCourseById(@Param("id") Long id);
 
     @Query("select c from Course c where c.deleted = false")
     List<Course> findAll(Sort sort);
--- App_Data/src/main/java/academy/belhard/lms/data/repository/HomeworkRepository.java
+++ App_Data/src/main/java/academy/belhard/lms/data/repository/HomeworkRepository.java
@@ -3,10 +3,11 @@ package academy.belhard.lms.data.repository;
 import academy.belhard.lms.data.entity.Homework;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface HomeworkRepository extends JpaRepository<Homework, Long> {
     @Query(value = "SELECT * FROM homeworks WHERE lesson_id = :id", nativeQuery = true)
-    List<Homework> findHomeworkByLessonId(Long id);
+    List<Homework> findHomeworkByLessonId(@Param("id") Long id);
 }
