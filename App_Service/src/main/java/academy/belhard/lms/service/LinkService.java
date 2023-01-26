aa27dfcdb7ef06555edc5c41c27747c75b191d9f
UladzislauM <marshal25@yandex.by>
2023-01-26 06:48:30 +0300

    LMS-39: Change name LinkService -> EmailLinkService

--- App_Service/src/main/java/academy/belhard/lms/service/LinkService.java
+++
@@ -1,9 +0,0 @@
-package academy.belhard.lms.service;
-
-public interface LinkService {
-    String tokenGenerated(int seconds);
-
-    boolean activated(String link);
-
-    boolean isActivated(String link);
-}
