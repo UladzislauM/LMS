fb539d14b4c8e5eaa7d4dfb057604e4143eef5ae
UladzislauM <marshal25@yandex.by>
2023-01-26 07:23:41 +0300

    LMS-39: Project Start

--- App_Service/src/main/generated/academy/belhard/lms/service/mapper/FileLinkMapperImpl.java
+++ App_Service/src/main/generated/academy/belhard/lms/service/mapper/FileLinkMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-26T06:45:09+0300",
+    date = "2023-01-26T07:01:16+0300",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.5 (Eclipse Adoptium)"
 )
 @Component
