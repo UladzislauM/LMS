1f1b6d88a54e71ecde1372b6880eb1b9638052e0
Herman Rabinkin <herman2lv@gmail.com>
2023-01-28 21:28:49 +0300

    LMS-47: add client side confirm password verification

---
+++ App_Web/src/main/resources/static/js/password-confirmation.js
@@ -0,0 +1,13 @@
+import {getNotification} from './notification.js';
+
+const password = $('#input-password').get(0);
+const passwordConfirmation = $('#input-confirm-password').get(0);
+const $submit = $('#input-submit');
+
+$submit.on('click', (e) => {
+  if (password.value !== passwordConfirmation.value) {
+    e.preventDefault();
+    const $notification = getNotification('Passwords do not match!');
+    $notification.display();
+  }
+});
