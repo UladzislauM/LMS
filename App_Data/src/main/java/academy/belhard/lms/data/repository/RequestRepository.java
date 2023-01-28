cfb1b562f82a9735789e921d9c74e0babb4ac7b3
UladzislauM <marshal25@yandex.by>
2023-01-28 08:39:52 +0300

    LMS-39: Add "findByStudent" to request repository

--- App_Data/src/main/java/academy/belhard/lms/data/repository/RequestRepository.java
+++ App_Data/src/main/java/academy/belhard/lms/data/repository/RequestRepository.java
@@ -2,10 +2,17 @@ package academy.belhard.lms.data.repository;
 
 import academy.belhard.lms.data.entity.Request;
 import academy.belhard.lms.data.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface RequestRepository extends JpaRepository<Request, Long> {
     Optional<Request> findByUser(User user);
+
+    @Query(value = "SELECT * FROM requests r WHERE r.user_id =:id", nativeQuery = true)
+    Page<Request> findByStudent(Pageable pageable, @Param("id") Long id);
 }
