363026e3cdac0aa07d4bc2901f888feabb22e3ac
Andrei3465 <a.khlapitski@gmail.com>
2023-01-31 23:27:35 +0300

    LMS-41: Add FileServiceWebController and view

--- App_Service/src/main/java/academy/belhard/lms/service/impl/FileServiceImpl.java
+++ App_Service/src/main/java/academy/belhard/lms/service/impl/FileServiceImpl.java
@@ -1,37 +1,44 @@
 package academy.belhard.lms.service.impl;
 
+import academy.belhard.lms.data.entity.FileLink;
+import academy.belhard.lms.data.repository.FileLinkRepository;
 import academy.belhard.lms.service.FileService;
+import academy.belhard.lms.service.dto.FileLinkDto;
+import academy.belhard.lms.service.mapper.FileLinkMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+@RequiredArgsConstructor
+@Service("fileService")
 public class FileServiceImpl implements FileService {
-    @Override
-    public void writeToFile(String fileAddress, String content) {
-        try (Writer writer = new OutputStreamWriter(new FileOutputStream(fileAddress))) {
-            writer.write(content);
-            writer.flush();
+    private static final String LOCAL_STORAGE_PATH = "files";
+    private final FileLinkRepository fileLinkRepository;
+    private final FileLinkMapper fileLinkMapper;
+
+    public FileLinkDto save(InputStream is) {
+        try {
+            String name = System.currentTimeMillis() + ".txt";
+            Path path = Path.of(LOCAL_STORAGE_PATH, name);
+            Files.copy(is, path);
+            FileLinkDto fileLinkDto = new FileLinkDto();
+            fileLinkDto.setLink(name);
+            fileLinkRepository.save(fileLinkMapper.fileLinkDtoToFileLink(fileLinkDto));
+            return fileLinkDto;
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
-    @Override
-    public String getFileContent(String fileAddress) {
-        StringBuilder content = new StringBuilder();
-        try (FileInputStream inputStream = new FileInputStream(fileAddress)) {
-            byte[] buffer = new byte[1000];
-            int read;
-            while ((read = inputStream.read(buffer)) != -1) {
-                content.append(new String(buffer, 0, read));
-                System.out.println(content);
-            }
+    public InputStream get(FileLink fileLink) {
+        try {
+            return Files.newInputStream(Path.of(LOCAL_STORAGE_PATH, fileLink.getLink()));
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return content.toString();
     }
 }
---
+++ App_Web/src/main/java/academy/belhard/lms/controller/view/FileServiceWebController.java
@@ -0,0 +1,41 @@
+package academy.belhard.lms.controller.view;
+
+import academy.belhard.lms.service.FileService;
+import academy.belhard.lms.service.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/uploader")
+public class FileServiceWebController {
+    public static final String FILE_UPLOAD_ERROR = "File upload error";
+    private final FileService fileService;
+
+    @GetMapping()
+    public String uploadForm() {
+        return "fileService";
+    }
+
+    @PostMapping()
+    public String upload(@RequestParam("file") MultipartFile multipartFile) {
+        try {
+            byte[] bytes = multipartFile.getBytes();
+            InputStream stream = new ByteArrayInputStream(bytes);
+            fileService.save(stream);
+        } catch (IOException e) {
+            throw new NotFoundException(FILE_UPLOAD_ERROR);
+        }
+        return "redirect:/";
+
+    }
+}
