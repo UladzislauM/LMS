cf3c663eae1a46b8ec02169cf60fe15a339a7d09
Кирилл Шевель <shevelkirill2011@gmail.com>
2023-02-01 18:21:34 +0300

    LMS-51: refactored HomeworkWebController.java and homeworks.html

--- App_Web/src/main/java/academy/belhard/lms/controller/view/HomeworkWebController.java
+++ App_Web/src/main/java/academy/belhard/lms/controller/view/HomeworkWebController.java
@@ -2,12 +2,16 @@ package academy.belhard.lms.controller.view;
 
 import academy.belhard.lms.service.FileLinkService;
 import academy.belhard.lms.service.HomeworkService;
+import academy.belhard.lms.service.UserService;
 import academy.belhard.lms.service.dto.course.HomeworkDto;
+import academy.belhard.lms.service.dto.user.UserDto;
+import academy.belhard.lms.service.impl.UserAppDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +26,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeworkWebController {
     private final HomeworkService homeworkService;
     private final FileLinkService fileLinkService;
+    private final UserService userService;
+
 
     @GetMapping
     public String getAll(Model model, @PageableDefault @SortDefault("id") Pageable pageable) {
@@ -38,7 +44,10 @@ public class HomeworkWebController {
     }
 
     @GetMapping("/create")
-    public String createForm() {
+    public String createForm(Model model) {
+        UserAppDetails userAppDetails = (UserAppDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDto userDto = userService.getById(userAppDetails.getId());
+        model.addAttribute("user", userDto);
         return "create_homework";
     }
 
