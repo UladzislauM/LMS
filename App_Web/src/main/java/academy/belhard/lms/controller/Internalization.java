39a0a3ca5cd75086f4b5a37d55aaf19e0f8e082a
Aminaev Roman <romanzovut@gmail.com>
2023-02-01 21:25:56 +0300

    LMS-50 Refactored Internalization.java.

--- App_Web/src/main/java/academy/belhard/lms/controller/Internalization.java
+++ App_Web/src/main/java/academy/belhard/lms/controller/Internalization.java
@@ -5,27 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class Internalization implements WebMvcConfigurer {
 
-    @Bean  //FIXME //Add switch to navbar
+    @Bean
     public LocaleResolver localeResolver() {
-        //For session
-//        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-//        sessionLocaleResolver.setDefaultLocale(Locale.US);
-//        return sessionLocaleResolver;
-
-        //If no session supported
-        CookieLocaleResolver resolver = new CookieLocaleResolver();
-//        resolver.setDefaultLocale(Locale.US);
-        resolver.setCookieDomain("localhost");
-
-        // 60 minutes
-        resolver.setCookieMaxAge(60 * 60);
-        return resolver;
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        return sessionLocaleResolver;
     }
 
     @Bean
