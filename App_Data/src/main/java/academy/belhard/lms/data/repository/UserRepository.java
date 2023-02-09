fd4a181293d30a4c5d3c848380946d54056453e5
nester77 <alex.nester2477@gmail.com>
2023-02-09 20:55:08 +0300

    LMS-76: Fix bug with case of email

--- App_Data/src/main/java/academy/belhard/lms/data/repository/UserRepository.java
+++ App_Data/src/main/java/academy/belhard/lms/data/repository/UserRepository.java
@@ -10,9 +10,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    @Query(value = "SELECT * FROM users u WHERE LOWER(u.email) = LOWER(:email)", nativeQuery = true)
+    Optional<User> findByEmail(@Param("email") String email);
 
-    @Query(value = "SELECT * FROM users u WHERE u.email =:email AND u.is_active = true", nativeQuery = true)
+    @Query(value = "SELECT * FROM users u WHERE LOWER(u.email) = LOWER(:email) AND u.is_active = true", nativeQuery = true)
     Optional<User> findByEmailActive(@Param("email") String email);
 
     @Query(value = "SELECT * FROM users u WHERE u.id in " +
