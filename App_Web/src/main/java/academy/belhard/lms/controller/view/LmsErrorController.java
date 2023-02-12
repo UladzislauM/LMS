52928eec3fb101684add91e02c05a23d3f9b8096
Aminaev Roman <romanzovut@gmail.com>
2023-02-12 11:38:12 +0300

    LMS-58 Refactored LmsErrorController. Fixing needed

--- App_Web/src/main/java/academy/belhard/lms/controller/view/LmsErrorController.java
+++ App_Web/src/main/java/academy/belhard/lms/controller/view/LmsErrorController.java
@@ -2,6 +2,10 @@ package academy.belhard.lms.controller.view;
 
 import academy.belhard.lms.service.exception.LmsException;
 import academy.belhard.lms.service.exception.NotFoundException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Map;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -11,34 +15,50 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @ControllerAdvice
 @RequestMapping("/error")
-public class ErrorController {
+public class LmsErrorController implements ErrorViewResolver {
+
+
     @GetMapping
     public String error() {
         return "error";
     }
 
-    @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String internalServerError(LmsException e, Model model) {
+    @ExceptionHandler(value = {LmsException.class})
+    public String internalServerErrorLms(LmsException e, Model model) {
         model.addAttribute("message", e.getMessage());
         return "error";
     }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = {ServletException.class, IllegalArgumentException.class})
+    public String internalServerErrorOther(LmsException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+
+
     @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DataIntegrityViolationException.class)
     public String conflictError(LmsException e, Model model) {
         model.addAttribute("message", e.getMessage());
         return "error";
     }
 
-    @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
     public String notFoundError(NotFoundException e, Model model) {
         model.addAttribute("message", e.getMessage());
         return "error";
+    }
+
+    @Override
+    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+        return null;  //FIXME ?//This method must be configured for others exceptions
     }
 }
